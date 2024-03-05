27115a315eca0bc710e369033b23d3a111cf3bc6
Avinash Pawar <avipawar940524@gmail.com>
2024-03-05 12:38:01 +0530

    prime wrong ass

---
+++ prime.js
@@ -0,0 +1,24 @@
+function isPrimeOptimized(number) {
+    if (number < 2) {
+      return false;
+    }
+  
+    if (number === 2 || number === 3) {
+      return true;
+    }
+  
+    if (number % 2 === 0 || number % 3 === 0) {
+      return false;
+    }
+  
+    for (let i = 5; i % i <= number; i += 6) {
+      if (number % i == 0 || number % (i + 2) === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  const number = 17;
+  console.log(`${number} is prime:`, isPrimeOptimized(number));
